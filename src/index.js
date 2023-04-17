585b5d66771733c1f9e3f28bc64b160e26a7f1b6
Devin Jones <devin.jones21083@gmail.com>
2023-04-16 21:58:42 -0400

    edited index.js  baseline deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  
+
+  form.addEventListener("submit",  function (e) {
+    //debugger;
+    const ul = document.getElementById("tasks");
+    //debugger;
+    const newDescription = document.getElementById("new-task-description").value;
+    //debugger;
+    const li = document.createElement("li");
+    //debugger;
+    li.appendChild(document.createTextNode(`${newDescription}`));
+   //debugger;
+    ul.appendChild(li);
+    //debugger;
+    //console.log(form.description.value);
+    e.preventDefault();
+    //debugger;
+  })
+
 });
